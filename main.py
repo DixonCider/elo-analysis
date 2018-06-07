00dd1dea7270bcaccdf979825438844f561f5d63
Andrew <baiandrew2010@gmail.com>
2018-06-07 12:25:07 +0800

    add get_theoretical_elo, get_average_elo functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import *
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+from functools import reduce
 
 POPULATION = 30
 INTRAPLAYER_SIGMA = 10
@@ -28,6 +30,9 @@ class Player(object):
     def record_elo(self):
         self.elo_record.append(self.elo)
 
+    def get_average_elo(self, last_n_percent):
+        averaging_range = math.floor(last_n_percent * len(self.elo_record))
+        return np.average(self.elo_record[averaging_range:]) 
 
 def update_elo(winner, loser, tie=False):
     Qa = pow(10, winner.elo/400)
@@ -68,7 +73,7 @@ def calc_theoretical_elo_diff(player1, player2):
     Returns player1_theoretical_elo - player2_theoretical_elo
     """
     def normal_cdf(x, mean, sigma):
-        return 0.5 * (1 + math.erf( (x-mean)/(sigma * 1.41421356237) ))
+        return 0.5 * (1 + math.erf( (x-mean)/(sigma * math.sqrt(2)) ))
     
     # Z = X - Y
     # X : (random variable) player1 strength.
@@ -82,6 +87,21 @@ def calc_theoretical_elo_diff(player1, player2):
     p1_winrate = 1 - p2_winrate 
 
     return (math.log(p1_winrate, 10) - math.log(p2_winrate, 10)) * 400
+
+def get_theoretical_elo(player, player_list):
+    N = len(player_list)
+
+    # Calculate sum of player elo difference.
+    elo_diff_sum = 0
+    for opponent in player_list:
+        if opponent == player:
+            continue
+        elo_diff_sum += calc_theoretical_elo_diff(player, opponent)
+
+    average_elo_diff_with_player = float(elo_diff_sum)/N
+    average_elo = float(sum(player.elo for player in player_list))/N
+
+    return average_elo + average_elo_diff_with_player
 
 def main():
     player_list = [Player(i, normalvariate(1000, INTERPLAYER_SIGMA), INIT_ELO) for i in range(POPULATION)]
@@ -117,8 +137,10 @@ def main():
 
     ## test diff
     print('#' * 20)
-    print('player0-player1={}, theoretical={}'.format(player_list[0].elo - player_list[1].elo,\
+    print('player0-player1={}, theoretical={}'.format(player_list[0].get_average_elo(0.1) - player_list[1].get_average_elo(0.1),\
      calc_theoretical_elo_diff(player_list[0], player_list[1])))
+    print('player0.elo={}, theoretical={}'.format(player_list[0].elo,\
+     get_theoretical_elo(player_list[0], player_list)))
 
     plt.show()
 
